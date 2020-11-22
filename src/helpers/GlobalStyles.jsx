9703f2a79dd6b3d7de95c0e4451d72e76163a64e
FernandoQ <octavius.sys@gmail.com>
2020-11-22 18:34:51 -0500

    inicio de proyecto

---
+++ src/helpers/GlobalStyles.jsx
@@ -0,0 +1,15 @@
+import {createGlobalStyle} from 'styled-components'
+
+const Global=createGlobalStyle`
+*{
+margin:0;
+padding:0;
+box-sizing:border-box;
+}
+body{
+/*     font-family: 'Kumbh Sans', sans-serif;
+ */    font-size:18px;
+}
+`;
+
+export default Global
